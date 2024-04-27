dca59c014d5f7ea025f748321056ae1dcdf3a795
Gyeony95 <rlg1133@naver.com>
2024-04-27 15:13:32 +0900

    feat : 변수 정상적으로 들어오는부분 확인

--- mass_finder_app/views.py
+++ mass_finder_app/views.py
@@ -4,6 +4,8 @@ from mass_finder_app.models import Test, AminoData
 from mass_finder_app.serializers import TestSerializers
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import logging
+import json
 
 
 # Create your views here.
@@ -16,21 +18,19 @@ class TestViewSet(viewsets.ModelViewSet):
 
 @csrf_exempt
 def calc_mass(request):
+    logging.basicConfig(level=logging.DEBUG)  # 모든 DEBUG 이상의 로그를 캡처
     if request.method == 'POST':
-        total_weight = request.POST.get('totalWeight')  # double
-        init_amino = request.POST.get('initAmino')  # String
-        current_formy_type = request.POST.get('currentFormyType')  # String
-        current_ion_type = request.POST.get('currentIonType')  # String
-        input_aminos = request.POST.get('inputAminos')  # Map<String, double>
-        user = AminoData(
-            total_weight=total_weight,
-            init_amino=init_amino,
-            current_formy_type=current_formy_type,
-            current_ion_type=current_ion_type,
-            input_aminos=input_aminos,
-        )
-        if user is not None:
-            return JsonResponse({'email': user.email}, status=200)
-        else:
-            return JsonResponse({'error': 'Invalid credentials'}, status=400)
-    return JsonResponse({'error': 'Invalid request'}, status=400)
+        data = json.loads(request.body.decode('utf-8'))  # request.body는 bytes형태이므로 decode 필요
+        total_weight = data.get('totalWeight')  # float
+        init_amino = data.get('initAmino')  # string
+        current_formy_type = data.get('currentFormyType')  # string
+        current_ion_type = data.get('currentIonType')  # string
+        input_aminos = data.get('inputAminos')  # dict
+        logging.debug(
+            f"total_weight : {total_weight}, init_amino : {init_amino}, current_formy_type : {current_formy_type}, current_ion_type : {current_ion_type}, input_aminos : {input_aminos}")
+        # if user is not None:
+        #     return JsonResponse({'email': user.email}, status=200)
+        # else:
+        #     return JsonResponse({'error': 'Invalid credentials'}, status=400)
+    # return JsonResponse({'error': 'Invalid request'}, status=400)
+    return JsonResponse({'success': 'calc compleate : '}, status=200)
